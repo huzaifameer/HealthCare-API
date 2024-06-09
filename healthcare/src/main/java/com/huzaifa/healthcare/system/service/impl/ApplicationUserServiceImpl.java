3669e484afa1114639ee0ebd35c6bc694cbea6c2
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-06-09 12:47:45 +0530

    commit-Day-2 Security-1

--- healthcare/src/main/java/com/huzaifa/healthcare/system/service/impl/ApplicationUserServiceImpl.java
+++ healthcare/src/main/java/com/huzaifa/healthcare/system/service/impl/ApplicationUserServiceImpl.java
@@ -1,16 +1,37 @@
 package com.huzaifa.healthcare.system.service.impl;
 
+import com.huzaifa.healthcare.system.entity.User;
+import com.huzaifa.healthcare.system.entity.UserRoleHasUser;
 import com.huzaifa.healthcare.system.repo.UserRepo;
 import com.huzaifa.healthcare.system.repo.UserRoleHasUserRepo;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Service
-public class ApplicationUserServiceImpl {
+public class ApplicationUserServiceImpl implements UserDetailsService {
     private final UserRepo userRepo;
     private final UserRoleHasUserRepo userRoleHasUserRepo;
 
     public ApplicationUserServiceImpl(UserRepo userRepo, UserRoleHasUserRepo userRoleHasUserRepo) {
         this.userRepo = userRepo;
         this.userRoleHasUserRepo = userRoleHasUserRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User selectedUser = userRepo.findByUserName(username);
+        if (selectedUser==null){
+            throw new UsernameNotFoundException(String.format("Username %s not found..!",username));
+        }
+
+        List<UserRoleHasUser> userRoles = userRoleHasUserRepo.findByUserId(selectedUser.getId());
+        Set<SimpleGrantedAuthority> grantedAuthorities = new HashSet<>();
     }
 }
